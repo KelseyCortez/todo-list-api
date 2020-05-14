204a4634f55a885bc5b4451e64619ae63aff43d4
KelseyCortez <kelscortez@gmail.com>
2020-05-14 16:38:11 -0400

    finishes exercises

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const port = 3000;
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('./public'));
 
-const todoList = [
+let todoList = [
   {
     id: 1,
     todo: 'Implement a REST API',
@@ -28,14 +29,87 @@ const todoList = [
 
 // GET /api/todos
 
+app.get('/api/todos/', (req, res) => {
+  console.log('got a request');
+  res.json(todoList);
+});
+
 // GET /api/todos/:id
 
+app.get('/api/todos/:id', (req, res) => {
+  const todo =
+    todoList.find((todo) => {
+      return todo.id === Number.parseInt(req.params.id);
+    }) || {};
+  const status = Object.keys(todo).length ? 200 : 404;
+  res.status(status).json(todo);
+});
 // POST /api/todos
 
+app.post('/api/todos', (req, res) => {
+  if (req.body.todo) {
+    const maxId = todoList.reduce((max, currentTodo) => {
+      if (currentTodo.id > max) {
+        max = currentTodo.id;
+      }
+      return max;
+    }, 0);
+
+    const newToDo = {
+      id: maxId + 1,
+      todo: req.body.todo,
+    };
+    todoList.push(newToDo);
+    res.json(newToDo);
+  } else {
+    res.status(400).json({
+      error: 'Provide todo text'
+
+
+    });
+  }
+});
+
 // PUT /api/todos/:id
+app.put('/api/todos/:id', (req, res) => {
+  if (!req.body || !req.body.todo) {
+    res.status(400).json({
+      error: 'Provide todo text',
+    });
+    return;
+  }
+  let updatedTodo = {};
+  todoList.forEach((todo) => {
+    if (todo.id === Number.parseInt(req.params.id)) {
+      todo.todo = req.body.todo;
+      updatedTodo = todo;
+
+    }
+  });
+  const status = Object.keys(updatedTodo).length ? 200 : 404;
+  res.status(status).json(updatedTodo);
+
+
+});
 
 // DELETE /api/todos/:id
 
-app.listen(3000, function () {
-  console.log('Todo List API is now listening on port 3000...');
+app.delete('/api/todos/:id', (req, res) => {
+  let found = false;
+  todoList = todoList.filter((todo) => {
+    if (todo.id === Number.parseInt(req.params.id)) {
+      found = true;
+      return false;
+
+    }
+
+    return true;
+  });
+  const status = found ? 200 : 404;
+  res.status(status).json(todoList);
 });
+
+app.listen(port, () => console.log(`listening on port http://127.0.0.1:${port}`));
+
+
+
